68e894c65c14c573f2dac9e7682fe192caa755d8
ismiwrdnta <ismiwrdnta@gmail.com>
2022-03-07 22:05:01 +0700

    menambahkan implementasi dengan table-driven

---
+++ tpmodul3_1302201135/tpmodul3_1302201135/KodePos.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using tpmodul3_1302201135;
+
+namespace tpmodul3_1302201135
+{
+	public class KodePos
+	{
+		public string getKodePos(string a)
+		{
+			string kode;
+			if (a == "Batununggal")
+			{
+				kode = "40266";
+			}
+			else if (a == "Kujangsari")
+			{
+				kode = "40287";
+			}
+			else if (a == "Mengger")
+			{
+				kode = "40267";
+			}
+			else if (a == "Wates")
+			{
+				kode = "40256";
+			}
+			else if (a == "Cijaura")
+			{
+				kode = "40287";
+			}
+			else if (a == "Jatisari")
+			{
+				kode = "40286";
+			}
+			else if (a == "Margasari")
+			{
+				kode = "40286";
+			}
+			else if (a == "Sekejati")
+			{
+				kode = "40286";
+			}
+			else if (a == "Kebonwaru")
+			{
+				kode = "40272";
+			}
+			else if (a == "Maleer")
+			{
+				kode = "40274";
+			}
+			else
+			{
+				kode = "40273";
+			}
+			return kode;
+		}
+	}
+}
--- tpmodul3_1302201135/tpmodul3_1302201135/Program.cs
+++ tpmodul3_1302201135/tpmodul3_1302201135/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace tpmodul3_1302201135
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            KodePos KP   = new KodePos();
+            Console.WriteLine("Kelurahan" + "     " + "Kode Pos");
+            Console.WriteLine("Batununggal" + "    " + KP.getKodePos("Batununggal"));
+            Console.WriteLine("Kujangsari" + "     " + KP.getKodePos("Kujangsari"));
+            Console.WriteLine("Mengger" + "        " + KP.getKodePos("Mengger"));
+            Console.WriteLine("Wates" + "          " + KP.getKodePos("Wates"));
+            Console.WriteLine("Cijaura" + "        " + KP.getKodePos("Cijaura"));
+            Console.WriteLine("Jatisari" + "       " + KP.getKodePos("Jatisari"));
+            Console.WriteLine("Margasari" + "      " + KP.getKodePos("Margasari"));
+            Console.WriteLine("Sekejati" + "       " + KP.getKodePos("Sekejati"));
+            Console.WriteLine("Kebonwaru" + "      " + KP.getKodePos("Kebonwaru"));
+            Console.WriteLine("Maleer" + "         " + KP.getKodePos("Maleer"));
+            Console.WriteLine("Samoja" + "         " + KP.getKodePos("Samoja"));
+        }
+    }
+
+    
+}
